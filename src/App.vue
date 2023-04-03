0b314440d2822a8926ee80f2fba371ee32137790
GabrielLima2803 <gabriellima2803@gmail.com>
2023-04-03 11:53:58 -0300

    vai trabaiar

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const nome = ref('')
+const senha = ref('')
+const senhaConfirmar = ref('')
+const data = ref(0)
+
+</script>
+
+<template>
+<div class="container">
+
+<form action="">
+    <input type="text" v-model="nome" placeholder="Nome" required/>
+    <input type="password" v-model="senha" placeholder="Senha" required/>
+    <input type="password" v-model="senhaConfirmar" placeholder="Confirmar senha" required/>
+    <input type="date" v-model="data" placeholder="Data de Nascimento" required/>
+
+</form>
+    <!-- div .container -->
+</div>
+</template>
+
+<style scoped>
+
+</style>
