da45bd60e77736f2047912748169e0f380b327e9
GabrielLima2803 <gabriellima2803@gmail.com>
2023-04-14 16:58:31 -0300

    Terminado

--- src/App.vue
+++ src/App.vue
@@ -13,79 +13,110 @@ const linguagemP = ref('')
 const biografia = ref('')
 const enviar = ref(false)
 
-
-
-
-
 </script>
 
 <template>
-<div class="container">
+  <div class="container">
     <div class="info">
-<form action="">
-    <p>Nome:</p>
-    <input type="text" v-model="nome" placeholder="Nome" required/>
-    <p>Senha</p>
-    <input type="password" v-model="senha" placeholder="Senha" required/>
-    <p>Confirmar senha</p>
-    <input type="password" v-model="senhaConfirmar" placeholder="Confirmar senha" required/>
-    <p>Data de nascimento</p>
-    <input type="date" v-model="data" placeholder="Data de Nascimento" required/>
-    <p>Endereço</p>
-    <input type="text" v-model="endereço" placeholder="Endereço" required/>
-    <p>Cidade</p>
-    <input type="text" v-model="cidade" placeholder="Cidade" required/>
-    <p>Estado</p>
-    <select v-model="estado" placeholder="AC">
-    <option value="AC">AC</option>
-    <option value="AL">AL</option>
-    <option value="AP">AP</option>
-    <option value="AM">AM</option>
-    <option value="BA">BA</option>
-    <option value="CE">CE</option>
-    <option value="DF">DF</option>
-    <option value="ES">ES</option>
-    <option value="GO">GO</option>
-    <option value="MA">MA</option>
-    <option value="MT">MT</option>
-    <option value="MS">MS</option>
-    <option value="MG">MG</option>
-    <option value="PA">PA</option>
-    <option value="PB">PB</option>
-    <option value="PR">PR</option>
-    <option value="PE">PE</option>
-    <option value="PI">PI</option>
-    <option value="RJ">RJ</option>
-    <option value="RN">RN</option>
-    <option value="RS">RS</option>
-    <option value="RO">RO</option>
-    <option value="RR">RR</option>
-    <option value="SC">SC</option>
-    <option value="SP">SP</option>
-    <option value="SE">SE</option>
-    <option value="TO">TO</option>
-  </select>
-  <p style="white-space: pre-line;">Hobbies</p>
-  <textarea v-model="hobbies"/>
-  <p>Linguagem de Programação</p>
-  <input class="radioCss" type="radio" v-model="linguagemP" value="JavaScript" /> JavaScript
-  <input class="radioCss" type="radio" v-model="linguagemP" value="Python" /> Python
-  <input class="radioCss" type="radio" v-model="linguagemP" value="C" /> C#
-  <input class="radioCss" type="radio" v-model="linguagemP" value="Php" /> Php
-  <input class="radioCss" type="radio" v-model="linguagemP" value="SQL" /> SQL
-  <input class="radioCss" type="radio" v-model="linguagemP" value="R" /> R
-  <input class="radioCss" type="radio" v-model="linguagemP" value="VBA" /> VBA
-  <p style="white-space: pre-line;">Biografia</p>
-  <textarea v-model="biografia"/>
-  <hr>
-  <button type="submit" @click="enviar = !enviar">Salvar informações</button>
-  <div v-if="enviar">
-    {{ nome }}
-  </div>
-</form>
-</div>
+      <form action="" @submit.prevent="">
+        <h1>Formulario</h1>
+        <hr>
+        <p>Nome:</p>
+        <input type="text" v-model="nome" placeholder="Nome" required />
+        <p>Senha:</p>
+        <input type="password" v-model="senha" placeholder="Senha" required />
+        <p>Confirmar senha:</p>
+        <input type="password" v-model="senhaConfirmar" placeholder="Confirmar senha" required />
+        <p>Data de nascimento:</p>
+        <input type="date" v-model="data" placeholder="Data de Nascimento" required />
+        <p>Endereço:</p>
+        <input type="text" v-model="endereço" placeholder="Endereço" required />
+        <p>Cidade:</p>
+        <input type="text" v-model="cidade" placeholder="Cidade" required />
+        <p>Estado:</p>
+        <select v-model="estado" placeholder="AC">
+          <option value="AC">AC</option>
+          <option value="AL">AL</option>
+          <option value="AP">AP</option>
+          <option value="AM">AM</option>
+          <option value="BA">BA</option>
+          <option value="CE">CE</option>
+          <option value="DF">DF</option>
+          <option value="ES">ES</option>
+          <option value="GO">GO</option>
+          <option value="MA">MA</option>
+          <option value="MT">MT</option>
+          <option value="MS">MS</option>
+          <option value="MG">MG</option>
+          <option value="PA">PA</option>
+          <option value="PB">PB</option>
+          <option value="PR">PR</option>
+          <option value="PE">PE</option>
+          <option value="PI">PI</option>
+          <option value="RJ">RJ</option>
+          <option value="RN">RN</option>
+          <option value="RS">RS</option>
+          <option value="RO">RO</option>
+          <option value="RR">RR</option>
+          <option value="SC">SC</option>
+          <option value="SP">SP</option>
+          <option value="SE">SE</option>
+          <option value="TO">TO</option>
+        </select>
+        <p style="white-space: pre-line;">Hobbies</p>
+        <textarea v-model="hobbies" />
+        <p>Linguagem de Programação:</p>
+        <input class="radioCss" type="radio" v-model="linguagemP" value="JavaScript" /> JavaScript
+        <input class="radioCss" type="radio" v-model="linguagemP" value="Python" /> Python
+        <input class="radioCss" type="radio" v-model="linguagemP" value="C#" /> C#
+        <input class="radioCss" type="radio" v-model="linguagemP" value="Php" /> Php
+        <input class="radioCss" type="radio" v-model="linguagemP" value="SQL" /> SQL
+        <input class="radioCss" type="radio" v-model="linguagemP" value="R" /> R
+        <input class="radioCss" type="radio" v-model="linguagemP" value="VBA" /> VBA
+        <p style="white-space: pre-line;">Biografia:</p>
+        <textarea v-model="biografia" />
+        <hr>
+        <button type="submit" @click="enviar = !enviar">Salvar informações</button>
+
+      </form>
+
+    </div>
     <!-- div .container -->
-</div>
+  </div>
+
+  <div class="container">
+    <div v-if="enviar" class="info">
+      <h1>Informações Salvas</h1>
+      <hr>
+      <div class="margemTexto">
+        Nome: {{ nome }}
+      </div>
+      <div class="margemTexto">
+        Senha: {{ senha }}
+      </div>
+      <div class="margemTexto">
+        Confirmar Senha: {{ senhaConfirmar }}
+      </div>
+      <div class="margemTexto">
+        Data de Nascimento: {{ data }}
+      </div>
+      <div class="margemTexto">
+        Endereço: {{ endereço }}
+      </div>
+      <div class="margemTexto">
+        Cidade: {{ cidade }}
+      </div>
+      <div class="margemTexto">
+        Hobbies: {{ hobbies }}
+      </div>
+      <div class="margemTexto">
+        Linguagem Programação: {{ linguagemP }}
+      </div>
+      <div class="margemTexto">
+        Biografia: {{ biografia }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -96,6 +127,7 @@ const enviar = ref(false)
   align-items: center;
   margin-top: 50px;
 }
+
 button {
   font-weight: bold;
   margin: 0 50px;
@@ -106,70 +138,41 @@ button {
   padding: 15px 50px;
   cursor: pointer;
 }
-.cor1{
-  color: green;
-  font-weight: bold;
-  margin: 50px 50px 50px 50px;
-  background-color: black;
-  border: none;
-  border-radius: 5px;
-  padding: 5px 10px;
-}
-.cor2{
-  color: red;
-  font-weight: bold;
-  margin: 50px 50px 50px 50px;
-  background-color: black;
-  border: none;
-  border-radius: 5px;
-  padding: 5px 10px;
-}
-.estilo{
-  font-weight: bold;
-  margin: 50px 50px 50px 50px;
-  background-color: black;
-  border: none;
-  border-radius: 5px;
-  padding: 5px 10px;
-  text-align: center;
-  width: 50%;
-}
+
 .info {
-  background-color: rgba(35,12,22,1);
+  background-color: rgba(35, 12, 22, 1);
   padding: 20px 30px;
   color: rgb(183, 210, 219);
   border-radius: 10px;
   text-align: center;
   margin: 10px;
+  width: 44%;
 }
-.green{
-  color: darkgreen;
-}
-.white {
-  color: white;
-}
-.red {
-  color: red;
-}
-.black{
-  color: black;
-}
-.border-radius{
+
+.border-radius {
   border-radius: 15px;
 }
-/* p{
+
+p {
   margin: 50px 50px 50px 50px;
-  
-} */
+
+}
+
 h1 {
   color: white;
   font-weight: bold;
   font-size: 1.5em;
 }
-p{
-    margin: 15px 15px 15px 15px;
+
+p {
+  margin: 15px 15px 15px 15px;
 }
-.radioCss{
-    margin-left: 15px;
+
+.margemTexto {
+  margin: 15px 15px 15px 15px;
+}
+
+.radioCss {
+  margin-left: 15px;
 }
 </style>
